ffb34261209389cc5659b99263924ac164ddbd7e
Bernardo Ramos Alonso Ribeiro <quenity@outlook.com>
2025-08-05 18:53:23 -0300

    feat: subindo crew

--- src/enem_crew/crew.py
+++ src/enem_crew/crew.py
@@ -1,64 +1,51 @@
-from crewai import Agent, Crew, Process, Task
-from crewai.project import CrewBase, agent, crew, task
-from crewai.agents.agent_builder.base_agent import BaseAgent
-from typing import List
-# If you want to run a snippet of code before or after the crew starts,
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+from crewai import Crew, Process
+from.agents import EnemAgents
+from.tasks import EnemTasks
+import os
 
-@CrewBase
-class EnemCrew():
-    """EnemCrew crew"""
+# Carregar variáveis de ambiente
+from dotenv import load_dotenv
+load_dotenv()
 
-    agents: List[BaseAgent]
-    tasks: List[Task]
+class EnemCrew:
+    def __init__(self, topic):
+        self.topic = topic
+        self.agents = EnemAgents()
+        self.tasks = EnemTasks()
 
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-    
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
-    @agent
-    def researcher(self) -> Agent:
-        return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
-            verbose=True
-        )
+    def run(self):
+        # Instanciar os agentes
+        pesquisador = self.agents.pesquisador_sociocultural()
+        redator = self.agents.redator_enem()
+        avaliador = self.agents.avaliador_especialista()
 
-    @agent
-    def reporting_analyst(self) -> Agent:
-        return Agent(
-            config=self.agents_config['reporting_analyst'], # type: ignore[index]
-            verbose=True
-        )
+        # Instanciar as tarefas
+        task_pesquisa = self.tasks.pesquisa_de_tema(pesquisador, self.topic)
+        task_escrita = self.tasks.escrita_da_redacao(redator, [task_pesquisa], self.topic)
+        task_avaliacao = self.tasks.avaliacao_da_redacao(avaliador, [task_escrita])
 
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
-    @task
-    def research_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['research_task'], # type: ignore[index]
-        )
-
-    @task
-    def reporting_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['reporting_task'], # type: ignore[index]
-            output_file='report.md'
-        )
-
-    @crew
-    def crew(self) -> Crew:
-        """Creates the EnemCrew crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
-
-        return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+        # Montar a Crew com um processo sequencial
+        crew = Crew(
+            agents=[pesquisador, redator, avaliador],
+            tasks=[task_pesquisa, task_escrita, task_avaliacao],
             process=Process.sequential,
-            verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+            verbose=2
         )
+
+        result = crew.kickoff()
+        return result
+
+# Ponto de entrada para execução
+if __name__ == "__main__":
+    print("## Bem-vindo à Equipe de IA para Redação do ENEM")
+    print('--------------------------------------------------')
+    topic_input = input("Qual é o tema da redação que você deseja trabalhar hoje?\n")
+    
+    enem_crew = EnemCrew(topic_input)
+    final_result = enem_crew.run()
+    
+    print("\n\n########################")
+    print("## Tarefa Concluída!")
+    print("########################\n")
+    print("Resultado Final:")
+    print(final_result)
